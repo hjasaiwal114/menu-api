421548c41a883fd69f4b3e80b3cba3d3601eec96
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-19 14:42:44 +0530

    adding configuration

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,26 @@ dotenv.config();
  * App Variables
  */
 
+if (!process.env.PORT) {
+    process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10)
+
+const app = express();
+
 /**
  *  App Configuration
  */
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
 /**
  * Server Activation
  */
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
