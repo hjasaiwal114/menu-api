97e3f85ac3b0e89f1f190cf424aab0961cfc9014
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-19 13:52:16 +0530

    jai shree ganesh

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Required External Modules
+ */
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+/**
+ * App Variables
+ */
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Server Activation
+ */
